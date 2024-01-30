ac5f4bfd997d4dc38e3f647329f69a2a2e0ee53a
Fernando-Henriquee <fernandomoiseswagner@gmail.com>
2024-01-30 19:14:00 -0300

    feat: add initial booking component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,32 @@
+import { ptBR } from "date-fns/locale";
 import Header from "../_components/header";
+import { format } from "date-fns";
+import Search from "./_components/search";
+import BookingItem from "../_components/booking-item";
 
 export default function Home() { 
     return (
         <div>
             <Header />
+
+            <div className="px-5 pt-5">
+                <h2 className="text-xl font-bold">Olá, Fernando</h2>
+                <p className="capitalize text-sm">
+                    {format(new Date(), "EEEE',' dd 'de' MMMM", { 
+                    locale: ptBR,
+                })}
+                </p>
+            </div>
+
+            <div className="px-5 mt-6">
+                <Search />
+            </div>
+
+            <div className="px-5 mt-6">
+
+                <h2 className="text-xs mb-3 font-bold uppercase text-gray-400">Agendamentos</h2>
+                <BookingItem />
+            </div>
         </div>
     )
 }

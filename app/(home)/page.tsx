b729eeee97415ea75d369afb4c72d0c03058815e
Fernando-Henriquee <fernandomoiseswagner@gmail.com>
2024-01-30 20:03:42 -0300

    feat: add initial barbershop item

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,8 +3,13 @@ import Header from "../_components/header";
 import { format } from "date-fns";
 import Search from "./_components/search";
 import BookingItem from "../_components/booking-item";
+import { db } from "../_lib/prisma";
+import BarberShopItem from "./_components/barbershop-item";
 
-export default function Home() { 
+export default  async function Home() { 
+
+    const barbershops = await db.barbershop.findMany({});
+
     return (
         <div>
             <Header />
@@ -26,6 +31,16 @@ export default function Home() {
 
                 <h2 className="text-xs mb-3 font-bold uppercase text-gray-400">Agendamentos</h2>
                 <BookingItem />
+            </div>
+
+            <div className="mt-6">
+            <h2 className="px-5 text-xs mb-3 font-bold uppercase text-gray-400">Recomendados</h2>
+
+                <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+                    {barbershops.map((barbershop) => (
+                        <BarberShopItem key={barbershop.id} barbershop={barbershop} />
+                    ))}
+                </div>
             </div>
         </div>
     )
--- app/_components/booking-item.tsx
+++ app/_components/booking-item.tsx
@@ -6,8 +6,8 @@ import { Card, CardContent } from "./ui/card";
 const BookingItem = () => {
     return ( 
         <Card>
-            <CardContent className="p-5 flex justify-between py-0">
-                <div className="flex flex-col gap-2 py-5">
+            <CardContent className="p-0 flex justify-between">
+                <div className="flex flex-col gap-2 p-5">
                     <Badge className="bg-[#221C3D] text-primary hover:bg-[#221C3D] w-fit">Confirmado</Badge>
                     <h2 className="font-bold">Corte de Cabelo</h2>
                     <div className="flex items-center gap-2">

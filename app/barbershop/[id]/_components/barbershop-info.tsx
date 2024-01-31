476c0839f751c9adc22b79314420e55767d40f44
Fernando-Henriquee <fernandomoiseswagner@gmail.com>
2024-01-30 23:10:14 -0300

    feat: add ServiceItem component

--- app/barbershop/[id]/_components/barbershop-info.tsx
+++ app/barbershop/[id]/_components/barbershop-info.tsx
@@ -20,31 +20,31 @@ const BarbershopInfo = ({barbershop}: BarbershopInfoProps) => {
 
     return ( 
         <div>
-        <div className="h-[250px] w-full relative">
-        <Button onClick={handleBackClick} size="icon" variant="outline" className="z-50 absolute top-4 left-4">
-            <ChevronLeftIcon />
-        </Button>
+            <div className="h-[250px] w-full relative">
+                <Button onClick={handleBackClick} size="icon" variant="outline" className="z-50 absolute top-4 left-4">
+                    <ChevronLeftIcon />
+                </Button>
 
-        <Button size="icon" variant="outline" className="z-50 absolute top-4 right-4">
-            <MenuIcon />
-        </Button>
+                <Button size="icon" variant="outline" className="z-50 absolute top-4 right-4">
+                    <MenuIcon />
+                </Button>
 
-            <Image src={barbershop?.imageUrl} fill alt={barbershop.name} style={{objectFit: "cover"}} className="opacity-75" />
-        </div>
-        <div className="px-5 py-3 pb-6 border-b border-solid border-secondary">
-            <h1 className="text-xl font-bold">{barbershop.name}</h1>
-            <div className="flex items-center gap-1 mt-2">
-                <MapPin className="text-primary" size={18} />
-                <p className="text-sm">{barbershop.address}</p>
+                <Image src={barbershop?.imageUrl} fill alt={barbershop.name} style={{objectFit: "cover"}} className="opacity-75" />
             </div>
+            <div className="px-5 py-3 pb-6 border-b border-solid border-secondary">
+                <h1 className="text-xl font-bold">{barbershop.name}</h1>
+                <div className="flex items-center gap-1 mt-2">
+                    <MapPin className="text-primary" size={18} />
+                    <p className="text-sm">{barbershop.address}</p>
+                </div>
 
-            <div className="flex items-center gap-1 mt-2">
-                <StarIcon className="text-primary" size={18} />
-                <p className="text-sm">4.8 (39 avaliações)</p>
+                <div className="flex items-center gap-1 mt-2">
+                    <StarIcon className="text-primary" size={18} />
+                    <p className="text-sm">4.8 (39 avaliações)</p>
+                </div>
+
             </div>
-
         </div>
-    </div>
      );
 }
  

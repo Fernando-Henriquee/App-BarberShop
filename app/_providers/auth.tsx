8699058635be33fdb7fb9fded31d36e9be6845e0
Fernando-Henriquee <fernandomoiseswagner@gmail.com>
2024-01-31 21:52:57 -0300

    feat: add sign in and sign out

--- app/_providers/auth.tsx
+++ app/_providers/auth.tsx
@@ -1,9 +1,9 @@
 "use client"; 
 
-import { SessionProvider } from 'next-auth/react';
+import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
 
-const AuthProvider = ({ children }: {children: ReactNode}) => {
+const AuthProvider = ({ children }: { children: ReactNode }) => {
     return <SessionProvider>{children}</SessionProvider>;
   
 };

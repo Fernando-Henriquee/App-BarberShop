63c88d08d704b8d66a82fd1cded932389ead98ef
Fernando-Henriquee <fernandomoiseswagner@gmail.com>
2024-02-02 19:21:34 -0300

    feat: add initial service booking

--- app/barbershop/[id]/page.tsx
+++ app/barbershop/[id]/page.tsx
@@ -1,47 +1,48 @@
-import { Button } from "@/app/_components/ui/button";
 import { db } from "@/app/_lib/prisma";
-import { ChevronLeftIcon, MapPin, MenuIcon, StarIcon } from "lucide-react";
-import Image from "next/image";
 import BarbershopInfo from "./_components/barbershop-info";
 import ServiceItem from "./_components/service-item";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-interface BarberShopDetailsPageProps {
-    params: {
-        id?: string;
-    };
+interface BarbershopDetailsPageProps {
+  params: {
+    id?: string;
+  };
 }
 
-const BarberShopDetailsPage = async ({params}: BarberShopDetailsPageProps) => {
-    if (!params.id) {
-        return null
-    }
+const BarbershopDetailsPage = async ({ params }: BarbershopDetailsPageProps) => {
+  const session = await getServerSession(authOptions);
 
-    const barbershop = await db.barbershop.findUnique ({
-        where: {
-            id: params.id
-        },
-        include: {
-            services: true
-        }
-    });
+  if (!params.id) {
+    // TODO: redirecionar para home page
+    return null;
+  }
 
-    if (!barbershop) {
-        return null
-    }
+  const barbershop = await db.barbershop.findUnique({
+    where: {
+      id: params.id,
+    },
+    include: {
+      services: true,
+    },
+  });
 
-    return ( 
-        <div>
-            <BarbershopInfo barbershop={barbershop} />
+  if (!barbershop) {
+    // TODO: redirecionar para home page
+    return null;
+  }
 
-            <div className="px-5 flex flex-col gap-4 py-6">
-                {barbershop.services.map((service) => ( 
-                    <ServiceItem key={service.id} service={service} />
-                ))}
-            </div>
-        </div>
-    )
-      
-    
-}
- 
-export default BarberShopDetailsPage;
+  return (
+    <div>
+      <BarbershopInfo barbershop={barbershop} />
+
+      <div className="px-5 flex flex-col gap-4 py-6">
+        {barbershop.services.map((service) => (
+          <ServiceItem key={service.id} barbershop={barbershop} service={service} isAuthenticated={!!session?.user} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BarbershopDetailsPage;

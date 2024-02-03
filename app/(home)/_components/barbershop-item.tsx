5fe7bb64c3834efce50696632c8bd488ae068321
Fernando-Henriquee <fernandomoiseswagner@gmail.com>
2024-02-03 00:31:57 -0300

    feat: barbershop search component

--- app/(home)/_components/barbershop-item.tsx
+++ app/(home)/_components/barbershop-item.tsx
@@ -8,39 +8,48 @@ import { StarIcon } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
-interface BarberShopItemProps {
-    barbershop: Barbershop;
+interface BarbershopItemProps {
+  barbershop: Barbershop;
 }
 
-const BarberShopItem = ({ barbershop }: BarberShopItemProps) => {
-    const router = useRouter();
+const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
+  const router = useRouter();
 
-    const handleBookingClick = () => { 
-        router.push(`/barbershop/${barbershop.id}`);
-     }
+  const handleBookingClick = () => {
+    router.push(`/barbershops/${barbershop.id}`);
+  };
 
-    return (
-        <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
-            <CardContent className="px-1 py-0">
-                <div className="w-full h-[159px] relative">
-                    <div className="absolute top-2 left-2 z-50" >
-                        <Badge variant="secondary" className="opacity-90 flex gap-1 items-center">
-                            <StarIcon size={12} className="fill-primary text-primary" />  
-                            <span className="text-xs">4.8</span>
-                        </Badge>
-                    </div>
-                    <Image alt={barbershop.name} src={barbershop.imageUrl} style={{ objectFit: "cover"}} fill height={0} width={0} sizes="100vw" className="rounded-2xl"/>
-                </div>
-                <div className="px-2 pb-3">
-                    <h2 className="font-bold mt-2 overflow-hidden text-allipsis text-nowrap">{barbershop.name}</h2>
-                    <h2 className="text-sm text-gray-400 overflow-hidden text-allipsis text-nowrap">{barbershop.address}</h2>
-                    <Button variant="secondary" className="w-full mt-3" onClick={handleBookingClick}>
-                        Reservar
-                    </Button>
-                </div>
-            </CardContent>
-        </Card>
-    );
-}
+  return (
+    <Card className="min-w-full max-w-full rounded-2xl">
+      <CardContent className="px-1 py-0 pt-1">
+        <div className="w-full h-[159px] relative">
+          <div className="absolute top-2 left-2 z-50">
+            <Badge variant="secondary" className="opacity-90 flex gap-1 items-center top-3 left-3">
+              <StarIcon size={12} className="fill-primary text-primary" />
+              <span className="text-xs">5,0</span>
+            </Badge>
+          </div>
+          <Image
+            alt={barbershop.name}
+            src={barbershop.imageUrl}
+            style={{
+              objectFit: "cover",
+            }}
+            fill
+            className="rounded-2xl"
+          />
+        </div>
 
-export default BarberShopItem;
+        <div className="px-2 pb-3">
+          <h2 className="font-bold mt-2 overflow-hidden text-ellipsis text-nowrap">{barbershop.name}</h2>
+          <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap">{barbershop.address}</p>
+          <Button className="w-full mt-3" variant="secondary" onClick={handleBookingClick}>
+            Reservar
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default BarbershopItem;
